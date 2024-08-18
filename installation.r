ff902b2407de5a64e4aab0a5dbde7e270a551eea
talhar123456 <48639541+talhar123456@users.noreply.github.com>
2024-08-18 20:02:14 +0200

    Add files via upload

---
+++ installation.r
@@ -0,0 +1,19 @@
+options(repos = c(CRAN = "https://cran.rstudio.com/"))
+
+install.packages("igraph")
+
+# Enter commands in R (or R studio, if installed)
+install.packages('Seurat')
+library(Seurat)
+
+setRepositories(ind = 1:3, addURLs = c('https://satijalab.r-universe.dev', 'https://bnprks.r-universe.dev/'))
+install.packages(c("BPCells", "presto", "glmGamPoi"))
+
+# Install the remotes package
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+install.packages('Signac')
+remotes::install_github("satijalab/seurat-data", quiet = TRUE)
+remotes::install_github("satijalab/azimuth", quiet = TRUE)
+remotes::install_github("satijalab/seurat-wrappers", quiet = TRUE)
